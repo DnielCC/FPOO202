ec479aecd4047b0c00c4f299800d432c70a673ca
DnielCC <123047063@upq.edu.mx>
2025-01-31 20:29:10 -0600

    Practica5ejercicio5

---
+++ Practica No.4/src/Practicano5ejercicio5.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Practicano5ejercicio5 {
+    public static void main(String[] args) {
+           String letra,palabra;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese la palabra: ");
+        letra = sc.nextLine();
+        System.out.println("Ingrese la letra para saber cuantas veces aparece: ");
+
+        for (i=)
+
+
+
+    }
+}
