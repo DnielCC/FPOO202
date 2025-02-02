e6caeef73cf09713028668c12f4886ee021a3689
DnielCC <123047063@upq.edu.mx>
2025-02-01 20:54:56 -0600

    Practica 5 ejercicio 5

--- Practica No.4/src/Practicano5ejercicio5.java
+++ Practica No.4/src/Practicano5ejercicio5.java
@@ -1,15 +1,23 @@
 import java.util.Scanner;
+
 public class Practicano5ejercicio5 {
     public static void main(String[] args) {
-           String letra,palabra;
+        String frase;
+        char letra;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese la palabra: ");
-        letra = sc.nextLine();
-        System.out.println("Ingrese la letra para saber cuantas veces aparece: ");
+        System.out.print("Ingresa una frase: ");
+         frase = sc.nextLine();
+        System.out.print("Ingresa una letra: ");
+         letra = sc.next().charAt(0);
 
-        for (i=)
+        int contador = 0;
 
 
-
+        for (int i = 0; i < frase.length(); i++) {
+            if (frase.charAt(i) == letra) {
+                contador++;
+            }
+        }
+        System.out.println("La letra '" + letra + "' aparece " + contador + " veces en la frase.");
     }
 }
