b47e9d30e705fe0d63d316936064621ab2fd18fa
DnielCC <123047063@upq.edu.mx>
2025-02-22 18:24:30 -0600

    Creacion del metodo para crear contraseñas

--- GENERACIONPASSWORDS/src/Passwords.java
+++ GENERACIONPASSWORDS/src/Passwords.java
@@ -1,20 +1,43 @@
-import javax.swing.JOptionPane;
-import java.util.Random;
+    import java.util.Random;
+    import javax.swing.*;
+    public class Passwords {
+       public String maylet="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public String minlet="abcdefghijklmnopqrst1234567890";
+        public String especial="@+*#%<>-!";
 
-public class Passwords {
+            public String  Generador(int length) {
+                int mayusc = JOptionPane.showConfirmDialog(null,"Desea que su password tenga letras mayusculas?","Pregunta", JOptionPane.YES_NO_OPTION);
+                    boolean validacionmayusculas = mayusc == JOptionPane.YES_OPTION;
+                int caracter = JOptionPane.showConfirmDialog(null, "Desea que su password tenga caracteres especiales?","Pregunta", JOptionPane.YES_NO_OPTION);
+                    boolean validacioncaracter = caracter == JOptionPane.YES_OPTION;
+                    if(validacionmayusculas){
+                        minlet += maylet;
+                    }
+                    if(validacioncaracter){
+                        minlet += especial;
+                    }
+                    char [] contraseña = new char[length];
+                    Random numrand = new Random();
+                    for (int i=0;i< length;i++){
+                        int index = numrand.nextInt(minlet.length());
+                        contraseña[i] = minlet.charAt(index);
 
-    int longitud;
+                    }
+                    return new String (contraseña);
 
-    public int ingresolongitud(){
-        JOptionPane.showMessageDialog(null, "Bienvenido.");
-         longitud=Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa la cantidad de digitos que tendra la contraseña: "));
-        return longitud;
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
     }
-
-
-
-
-
-
-
-}
