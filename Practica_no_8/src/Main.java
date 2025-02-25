f514189aeb92d9e9c5ff53b486f6b3ef0f73095e
DnielCC <123047063@upq.edu.mx>
2025-02-25 15:28:07 -0600

    implementación en el metodo main

--- Practica_no_8/src/Main.java
+++ Practica_no_8/src/Main.java
@@ -3,27 +3,21 @@ public class Main {
     public static void main(String[] args) {
 
         //creamos el primer objeto
-        Spartan masterChief = new Spartan();
-
-        // usamos sus atributos
-        masterChief.nombre="John";
-        masterChief.salud=100;
-        masterChief.escudo=60;
-        masterChief.armaPrincipal="Rifle de asalto";
+        Spartan masterChief = new Spartan("John",100,60,"rifle de asalto");
 
         //invocamos los metodos
         masterChief.mostrarInfo();
         masterChief.Atacar("Grunt");
         masterChief.RecargarArma(75);
         masterChief.Correr(true);
+        //uso del ger y set para cambiar atributos
+        masterChief.setNombre("Eros");
+        masterChief.mostrarInfo();
+        System.out.println(masterChief.getNombre());
+
 
         //Creamos el Segundo Objeto
-        Spartan Margarito = new Spartan();
-
-        Margarito.nombre="Margarito";
-        Margarito.salud=150;
-        Margarito.escudo=260;
-        Margarito.armaPrincipal="Pistola chiquita";
+        Spartan Margarito = new Spartan("Margarito",150,260,"una naranja");
 
         Margarito.mostrarInfo();
         Margarito.Atacar("Changoleon");
