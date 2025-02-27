17907830407071d75cdd2299a232e9afd72835fc
DnielCC <123047063@upq.edu.mx>
2025-02-27 15:43:20 -0600

    integracion en el metod main

--- GENERACIONPASSWORDS/src/Main.java
+++ GENERACIONPASSWORDS/src/Main.java
@@ -2,16 +2,14 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        Passwords primera=new Passwords();
+        Passwords primera=new Passwords("ABCDEFGHIJKLMNOPQRSTUVWXYZ ", "@+*#%<>-!", "abcdefghijklmnopqrstuvwxyz1234567890");
         int largo =8;
         String entradai=JOptionPane.showInputDialog("Ingrese la longitud del pasword: ");
         if (entradai != null && !(entradai.isEmpty()) ){
             largo=Integer.parseInt(entradai);
         }
         String contra= primera.Generador(largo);
-
               JOptionPane.showMessageDialog(null, "La password es:  "+ contra);
-
         primera.seguridad(contra);
 
 
