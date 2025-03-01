87ad60c8a32a797f6b6ba3fc9ffe7bebe825ef87
DnielCC <123047063@upq.edu.mx>
2025-03-01 13:01:39 -0600

    Adicion del conductor en la clase vehiculo

--- PRACTICA13/src/vehiculo.java
+++ PRACTICA13/src/vehiculo.java
@@ -3,43 +3,55 @@ import javax.swing.*;
 public class vehiculo {
 
     private String placa;
+    private String modelo;
+    private int capacidadcarga;
+
+    private conductor conductor;
 
     public vehiculo(String placa, String modelo, int capacidadcarga) {
         this.placa = placa;
         this.modelo = modelo;
         this.capacidadcarga = capacidadcarga;
+        this.conductor=null;
+    }
+    public vehiculo(conductor conductor) {
+        this.conductor = conductor;
     }
 
-    private String modelo;
-    private int capacidadcarga;
+    public void asigconductor() {
+        if (this.conductor != null) {
+            JOptionPane.showMessageDialog(null, "Conductor esta asignado");
+            return;
+        }
+        String nombre=JOptionPane.showInputDialog(null, "Ingrese el nombre del conductor: ");
+        String identificacion =JOptionPane.showInputDialog(null, "Ingrese la identificacion del conductor: ");
+        String licencia =JOptionPane.showInputDialog(null, "Ingrese la licencia del conductor: ");
+
+        this.conductor = new conductor(nombre, identificacion, licencia);
+        JOptionPane.showMessageDialog(null, "Conductor asignado. :)");
+    }
 
 
-
-
+      public conductor getConductor() {
+        return conductor;
+        }
     public String getModelo() {
         return modelo;
     }
-
     public void setModelo(String modelo) {
         this.modelo = modelo;
     }
-
     public String getPlaca() {
         return placa;
     }
-
     public void setPlaca(String placa) {
         this.placa = placa;
     }
-
     public int getCapacidadcarga() {
         return capacidadcarga;
     }
-
     public void setCapacidadcarga(int capacidadcarga) {
         this.capacidadcarga = capacidadcarga;
     }
-
-
 }
 
